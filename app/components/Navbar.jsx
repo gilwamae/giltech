1a2a8f8e8b2e64674bdfd7276ec67dc610bdcb76
gilwamae <gilwayne900@gmail.com>
2025-03-01 10:35:45 +0300

    fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -18,7 +18,7 @@ const navLinks = [
         title: "Contact",
         path: "#contact"
     }
-]
+];
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
