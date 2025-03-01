38accfd1cb72de63974865056fecc4f4b74cf91e
gilwamae <gilwayne900@gmail.com>
2025-03-01 11:00:01 +0300

    fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
         //Footer design
         <footer className="footer border z-10 border-t-[#33353F]  border-l-transparent border-r-transparent  text-white">
             <div className="container p-12 flex    justify-between">
-                <span>GILTECH</span>
-                <p className="text-slate-800">All rights reserved.</p>
+                <span>GIL WAMAE</span>
+                <p className="text-slate-800">All rights reserved 2025.</p>
             </div>
         </footer>
     )
